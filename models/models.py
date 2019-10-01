b75ce6e8d0ac8227bef66e98440ca1b845f0d27a
sarah <sweflorist@gmail.com>
2019-10-01 20:12:29 +0200

    [ADD] my_module: add the start_datetime field to the model my_module.my_module

--- models/models.py
+++ models/models.py
@@ -2,14 +2,15 @@
 
 from odoo import models, fields, api
 
-# class sarah_module(models.Model):
-#     _name = 'sarah_module.sarah_module'
+ class sarah_module(models.Model):
+     _name = 'sarah_module.sarah_module'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+     start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+
+     @api.depends('value')
+     def _value_pc(self):
+         self.value2 = float(self.value) / 100
